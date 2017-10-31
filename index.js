fa106978ccea3bfe8e165ddb12ddb1d0760ac512
antoniocordasco <cordasco.a@gmail.com>
2017-10-31 17:13:22 +0000

    added getNewOrientation() function

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var fs = require("fs");
 
+var width = 0;
+var height = 0;
+
+var orientations = ["N", "E", "S", "W"];
+var orientationValues = {
+  N: 0,
+  E: 1,
+  S: 2,
+  W: 3
+};
+
 fs.readFile("input.txt", "utf8", function(err, data) {
   if (err) throw err;
 
   var lines = data.split("\n");
   var sizes = lines[0].split(" ");
 
-  var width = parseInt(sizes[0]) + 1;
-  var height = parseInt(sizes[1]) + 1;
+  width = parseInt(sizes[0]) + 1;
+  height = parseInt(sizes[1]) + 1;
 
   var robotPositions = [];
   var robotInstructions = [];
@@ -23,9 +34,41 @@ fs.readFile("input.txt", "utf8", function(err, data) {
 
     robotPositions.push(pos);
     robotInstructions.push(lines[i + 1]);
+
+    processRobot(pos, lines[i + 1]);
   }
 
   console.log(width, height);
   console.log(robotPositions);
   console.log(robotInstructions);
 });
+
+// given a current orientation and direction, it returns the new orientation
+function getNewOrientation(orientation, dir) {
+  var val = orientationValues[orientation];
+
+  if (dir == "R") {
+    val++;
+  } else {
+    val--;
+  }
+
+  if (val == -1) {
+    val = 3;
+  } else if (val == 4) {
+      val = 0;
+  }
+
+  return orientations[val];
+}
+
+function processRobot(pos, instructions) {
+  console.log("pos", pos);
+  getNewOrientation(pos.orientation, "R");
+  getNewOrientation(pos.orientation, "L");
+
+  for (var i in instructions) {
+  }
+
+  console.log(width, height);
+}
