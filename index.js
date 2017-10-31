cc7007b8049cfd3da035e25126f14236893e7397
antoniocordasco <cordasco.a@gmail.com>
2017-10-31 18:40:13 +0000

    allowing for different text files to be used as input

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var fs = require("fs");
-var RobotsProcessor = require('./classes/RobotsProcessor');
+var RobotsProcessor = require("./classes/RobotsProcessor");
 
 var width = 0;
 var height = 0;
 
+var inputFile = "input.txt";
 
-fs.readFile("input.txt", "utf8", function(err, data) {
+
+// checking if input filename has been specified when running the process
+if ("undefined" !== process.argv[2]) {
+  inputFile = process.argv[2];
+}
+
+fs.readFile(inputFile, "utf8", function(err, data) {
   if (err) throw err;
 
   var p = new RobotsProcessor(data);
   p.processAndPrintAllRobots();
-
 });
-
